1e844d94f387512038b1ea6375abcd9d1eadc08e
TJ Stevens <tj@tj-Gazelle-Ultra.(none)>
2011-06-05 22:50:37 -0400

    new website

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DagobahWebsite::Application.config.secret_token = '43f3f5c65efcc018337b751fb263870b4c00800a4342b7a508edbe6acebb5b77dbe7502bf7c4ee9008b5211a6bacf99494fd5932a170d18846732108d1f59e33'
